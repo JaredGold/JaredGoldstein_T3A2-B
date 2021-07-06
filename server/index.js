b1e117f5071c9dfa9cff08f04c6456c942e8b721
JaredGold <jegoldstein94@gmail.com>
2021-07-06 19:54:17 +1000

    server set up

--- server/index.js
+++ server/index.js
@@ -1,4 +1,11 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://jegoldstein94:<password>@cluster0.0xaoc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
