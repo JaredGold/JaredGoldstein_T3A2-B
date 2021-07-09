c6dbe0ba17e865c6e2a7fa80c3c2178b5f840a96
JaredGold <jegoldstein94@gmail.com>
2021-07-09 13:13:26 +1000

    Add NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,36 @@
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>    
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact-us">
+            <Contact />
+          </Route>
+          <Route path="/sign-in">
+            <SignIn />
+          </Route>
+          <Route path="/sign-up">
+            <SignUp />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ client/src/components/NavBar/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link, useLocation } from "react-router-dom";
+import { Tabs, Tab, AppBar, Toolbar, Typography } from '@material-ui/core'
+
+const NavBar = () => {
+
+  const navLinks = [
+    {
+      label: "Home",
+      value: "/"
+    },
+    {
+      label: "About",
+      value: "/about"
+    },
+    {
+      label: "Contact",
+      value: "/contact-us"
+    },
+    {
+      label: "Sign In",
+      value: "/sign-in"
+    },
+    
+  ]
+
+  const {pathname} = useLocation()
+
+  return(
+    <AppBar position="sticky">
+      <Toolbar>
+        <Typography component="h1" variant="h6">BoxBox</Typography>
+        <Tabs value={pathname}>
+          {navLinks.map(({label, value}) => (
+            <Tab key={value} label={label} value={value} component={Link} to={value} />
+          ))}
+        </Tabs>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default NavBar;
